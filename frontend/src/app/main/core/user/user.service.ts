64453d6289a2b286f54e58d5bf1431b7fb66ef44
oTrolado <gabriel.costa41@fatec.sp.gov.br>
2019-08-16 14:49:08 -0300

    layout do login

--- frontend/src/app/main/core/user/user.service.ts
+++ frontend/src/app/main/core/user/user.service.ts
@@ -4,14 +4,14 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class UserService {
-    
+
     info = {
         id: null,
         name: null,
         level: null,
         user: null,
         auth: false,
-    }
+    };
 
     constructor() { }
 
@@ -23,5 +23,9 @@ export class UserService {
         return this.info.auth;
     }
 
-    
+    public login(info: {user: string, passw: string}): boolean {
+        if ( info.user === '123' && info.passw === '123') 
+            return true;
+        return false;
+    }
 }
--- frontend/src/app/main/shared/shared.module.ts
+++ frontend/src/app/main/shared/shared.module.ts
@@ -1,21 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 const material = [
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule
 ];
 
 @NgModule({
     declarations: [],
     imports: [
         CommonModule,
+        FormsModule,
         ...material
     ],
     exports: [
+        FormsModule,
+        CommonModule,
         ...material
     ]
 })
