098d87bc0f43b211d2b76e66eae6e87f244ec30a
diegorugue <diego.rugue@smn.com.br>
2019-08-20 14:43:48 -0300

    Add cors

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ const extensoes = require('./src/service/extensoes')
 const response = require('./src/service/middlewares/response')
 const usuarioRouter = require('./src/core/usuario/controller')
 const loginRouter = require('./src/core/login/controller')
+const cors = require('cors')
 
 const app = express()
 
 app.use(logger('dev'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
