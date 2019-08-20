33ee3c2f9d5b9a323e74b3e095509b35ab6bf121
diegorugue <diego.rugue@smn.com.br>
2019-08-20 17:08:00 -0300

    criptografando a senha

--- backend/src/core/login/controller.js
+++ backend/src/core/login/controller.js
@@ -3,15 +3,19 @@ const repository = require('./repository')
 const controller = require('../../service/middlewares/controller')
 const error = require('./service')
 const auth = require('../../service/auth')
+const bcrypt = require('bcrypt')
 
 module.exports = router
 
 router.post('/',
     controller( async (req, res, next) => {
-        const result = await repository.login(req.body)
+        const hashSenha = bcrypt.hashSync(req.body.senha, 10)
+        const result = await repository.login({...req.body, senha: hashSenha})
+
         if (result.success()) {
             const usuario = await repository.buscarPorEmail(req.body.email)
             const token = await auth.generateToken(usuario)
+
             res.ok({ token, content: usuario })
         } 
 
